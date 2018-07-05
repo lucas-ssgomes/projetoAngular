a24c88fd7f442ecec7ea2d94f1415b8dffba2b4d
Lucas Santos <lucas.lssg@gmail.com>
2018-07-05 11:13:57 -0300

    Adicionando arquivos do projeto

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    // tslint:disable-next-line:component-selector
+    selector: 'meu-primeiro-component',
+    template: `
+        <p>Meu primeiro component com Angular 6! (cries in desesperado language)</p>
+    `
+})
+export class MeuPrimeiroComponent {}
